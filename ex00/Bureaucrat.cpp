a6e87f028bc14039e1b9d6b03dac87a8cb7b6aea
Cmme <152791634+Crmorale@users.noreply.github.com>
2026-04-27 15:44:37 +0200

    Refactoring

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -14,26 +14,30 @@
 
 //CONSTRUCTORS
 Bureaucrat::Bureaucrat() : _name("default"), _grade(150) {
-	
+	std::cout << "Bureaucrat called " << _name << " has been created" << std::endl;
 }
 
-Bureaucrat::Bureaucrat(const std::string name, int grade) : _name(name) {
-	if (_grade < 1)
-		throw GradeTooLowException();
-	else if (_grade > 150)
+Bureaucrat::Bureaucrat(const std::string name, int grade) : _name(name), _grade(grade) {
+	if (grade < 1)
 		throw GradeTooHighException();
+	else if (grade > 150)
+		throw GradeTooLowException();
 	else
 		_grade = grade;
 }
 
-Bureaucrat::Bureaucrat(const Bureaucrat &other) : _grade(other._grade), _name(other._name){
-
+Bureaucrat::Bureaucrat(const Bureaucrat &other) : _name(other._name), _grade(other._grade) {
+	std::cout << "Bureaucrat copy constructor called" << std::endl;
 }
 
+Bureaucrat::~Bureaucrat() {}
+
 Bureaucrat	&Bureaucrat::operator=(const Bureaucrat &other) {
-		if (this != &other)
-		_grade = other._grade;
-	return (*this);
+		if (this != &other) {
+			_grade = other._grade;
+			std::cout << "Bureaucrat assignment operator called" << std::endl;
+		}
+			return (*this);
 }
 
 //GETTERS
@@ -62,3 +66,15 @@ void Bureaucrat::decrementGrade() {
 		_grade++;
 }
 
+const char* Bureaucrat::GradeTooHighException::what() const throw() {
+	return ("Exception: Grade is too high");
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw() {
+	return ("Exception: Grade is too low");
+}
+
+std::ostream	&operator<<(std::ostream &os, const Bureaucrat &b) {
+	os << b.getName() << " bureaucrat grade " << b.getGrade() << std::endl;
+	return (os);
+}
